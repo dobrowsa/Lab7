f650e7ce8d93fd470d7a11c73e06a0770199ebc4
dobrowsa <dobrowsa@mail.uc.edu>
2021-03-22 04:34:45 +0000

    Final Code Changes and Structure alterations

--- Counter.java
+++ Counter.java
@@ -2,67 +2,58 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class Counter implements ActionListener{
+public class Counter implements ActionListener {
+  int leftCount = 0;
+  int rightCount = 0;
 
-  JLabel leftLabel;
-  JLabel rightLabel; 
-  int leftCount;
-  int rightCount;
+  JLabel leftLabel = new JLabel("Count: " + leftCount);
+  JLabel rightLabel = new JLabel("Count: " + rightCount);
 
   // Creation of Constructor
   Counter() {
-    //Frame Specifications
+    // Frame Specifications
     JFrame frame = new JFrame("Let's Count");
     frame.setLayout(new FlowLayout());
-    frame.setSize(200,110);
+    frame.setSize(200, 110);
 
-    //Creation of Buttons
+    // Creation of Buttons
     JButton left = new JButton("Left");
     JButton right = new JButton("Right");
 
-    //Setting counts to 0
-    leftCount = 0;
-    rightCount = 0;
-
-    //Creation of Action Listeners
+    // Creation of Action Listeners
     left.addActionListener(this);
     right.addActionListener(this);
 
-    leftLabel.setText("Count " + leftLabel);
-    rightLabel.setText("Count " + rightLabel);
-
-    //Adding to Frame
-    frame.add(left);
-    frame.add(right);
-
-    //Creation of Reset Button
-    JButton reset;
-    reset = new JButton("Reset");
-
+    // Creation of Reset Button
+    JButton reset = new JButton("Reset");
     reset.addActionListener(this);
 
+    // Adding to Frame
+    frame.add(left);
+    frame.add(right);
+    frame.add(leftLabel);
+    frame.add(rightLabel);
     frame.add(reset);
 
-    //Setting Frame to Visible
+    // Setting Frame to Visible
     frame.setVisible(true);
   }
 
-  //Creation of Action Performed Method
+  // Creation of Action Performed Method
   public void actionPerformed(ActionEvent ae) {
     if(ae.getActionCommand().equals("Left")) {
       leftCount++;
-      leftLabel.setText("You pressed left");
+      leftLabel.setText("Count: " + leftCount);
     }
     else if (ae.getActionCommand().equals("Right")) {
       rightCount++;
-      rightLabel.setText("You pressed right");
+      rightLabel.setText("Count: " + rightCount);
     }
     else if (ae.getActionCommand().equals("Reset")) {
       leftCount = 0;
       rightCount = 0;
-      reset = JButton("Reset");
-      leftLabel.setText("Count " + leftLabel);
-      rightLabel.setText("Count " + rightLabel);
+      leftLabel.setText("Count: " + leftCount);
+      rightLabel.setText("Count: " + rightCount);
     }
   }
 }
